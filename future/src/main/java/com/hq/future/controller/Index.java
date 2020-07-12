0e4cf91527a4df8223dfb46dbd307c8489550621
hq <huangqing1@100tal.com>
2020-07-12 10:35:21 +0800

    ignore修改

--- future/src/main/java/com/hq/future/controller/Index.java
+++ future/src/main/java/com/hq/future/controller/Index.java
@@ -1,14 +1,32 @@
 package com.hq.future.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class Index {
     @RequestMapping("/index")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/music")
+    public String music(){
+        return "music";
+    }
+
+    @RequestMapping("/movie")
+    public String movie(){
+        return "movie";
+    }
+
+    @RequestMapping("/album")
+    public String album(){
+        return "album";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
     }
 }
